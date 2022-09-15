f6495c0c901a7e2e02fd714b778200b9385c75dd
ACHI9944 <achi9944@gmail.com>
2022-09-15 12:11:27 +0400

    Added password change and context on password change and logout

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,13 +1,17 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
-
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+
   const authCtx = useContext(AuthContext);
 
   const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logOut();
+  };
 
   return (
     <header className={classes.header}>
@@ -28,7 +32,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
